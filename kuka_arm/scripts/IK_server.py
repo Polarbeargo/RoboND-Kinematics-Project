3fedb1657dc0c8fce496bf5c51ab19ae51434b19
alice <bow1226@gmail.com>
2018-04-11 06:05:51 +0800

    Format
    Format

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -18,7 +18,6 @@ from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
 
-
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
     if len(req.poses) < 1:
@@ -26,22 +25,22 @@ def handle_calculate_IK(req):
         return -1
     else:
 
-        ### Your FK code here
+        # Your FK code here
         # Create symbols
-	#
-	#
-	# Create Modified DH parameters
-	#
-	#
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+        #
+        #
+        # Create Modified DH parameters
+        #
+        #
+        # Define Modified DH Transformation matrix
+        #
+        #
+        # Create individual transformation matrices
+        #
+        #
+        # Extract rotation matrices from the transformation matrices
+        #
+        #
         ###
 
         # Initialize service response
@@ -50,9 +49,9 @@ def handle_calculate_IK(req):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+            # Extract end-effector position and orientation from request
+            # px,py,pz = end-effector position
+            # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
@@ -61,23 +60,24 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 
-            ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
-	    # Calculate joint angles using Geometric IK method
-	    #
-	    #
+            # Your IK code here
+            # Compensate for rotation discrepancy between DH parameters and Gazebo
+            #
+            #
+            # Calculate joint angles using Geometric IK method
+            #
+            #
             ###
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+            joint_trajectory_point.positions = [
+                theta1, theta2, theta3, theta4, theta5, theta6]
+            joint_trajectory_list.append(joint_trajectory_point)
 
-        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+        rospy.loginfo("length of Joint Trajectory List: %s" %
+                      len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
-
 
 def IK_server():
     # initialize node and declare calculate_ik service
